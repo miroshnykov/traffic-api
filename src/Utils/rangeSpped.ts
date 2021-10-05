fa6204fef13b3ca2f523c9a18b90146ebb3a6e74
miroshnykov <dmytro.miroshnykov@actionmediamtl.com>
2021-10-05 12:13:40 -0400

    Spped time of request to metrics

---
+++ src/Utils/rangeSpped.ts
@@ -0,0 +1,14 @@
+export const rangeSpeed = (time: number) => {
+  switch (true) {
+    case time < 500 :
+      return 500
+    case  time > 501 && time < 1000 :
+      return 1000
+    case  time > 1001 && time < 1500 :
+      return 1500
+    case  time > 1501 && time < 2000 :
+      return 2000
+    default:
+      return 2500
+  }
+}

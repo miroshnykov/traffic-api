d04c13599f765f22c2d4edc094a10e9e99468808
miroshnykov <dmytro.miroshnykov@actionmediamtl.com>
2021-12-14 09:23:26 -0500

    some logs

--- src/Utils/cache.ts
+++ src/Utils/cache.ts
@@ -2,10 +2,12 @@ import consola from "consola";
 import {redis} from "../redis";
 import {influxdb} from "./metrics";
 import {ISqsMessage} from "../Interfaces/sqsMessage";
+import os from "os";
+const computerName = os.hostname()
 
 export const setSqsDataToRedis = async (message: ISqsMessage): Promise<void> => {
 
-  consola.info(`Getting from recipe-api update, Set to redis ${message.type}ID:${message.id}, action:${message.action}, comments:${message.comments} `)
+  consola.info(`Getting from recipe-api update, Set to redis ${message.type}ID:${message.id}, action:${message.action}, comments:${message.comments}, Computer name { ${computerName} }`)
   try {
     if (message.action === 'updateOrCreate') {
       await redis.set(`${message.type}:${message.id}`, message.body)

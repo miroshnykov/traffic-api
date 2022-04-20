3f29c94f112efec59da206b7c7c33e71ca221d3b
miroshnykov <dmytro.miroshnykov@actionmediamtl.com>
2022-04-20 17:39:25 -0400

    PH-881 fix  Default campaign

--- src/Interfaces/campaigns.ts
+++ src/Interfaces/campaigns.ts
@@ -6,10 +6,19 @@ export interface ICampaign {
   offerId: number
   affiliateId: number
   affiliateStatus: string
+  campaignStatus: string
   capSetup?: boolean
   capInfo?: ICapInfo
   payout: number
   payoutPercent: number
   affiliateManagerId: number
   targetRules?: object[]
+}
+
+export enum ICampaignStatus {
+  INACTIVE = 'inactive',
+  ACTIVE = 'active',
+  PENDING = 'pending',
+  BLOCKED = 'blocked',
+  REJECTED = 'rejected',
 }

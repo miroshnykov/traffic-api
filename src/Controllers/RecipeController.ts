1d717309662ff3b2deb56796a1ceb85ac8d98a9e
dmytro miroshnykov <dmytro.miroshnykov@milkbox.com>
2022-04-27 22:00:39 -0400

    del redis key

--- src/Controllers/RecipeController.ts
+++ src/Controllers/RecipeController.ts
@@ -5,7 +5,7 @@ import { recipeCampaignsServices } from '../Services/recipeCampaignsServices';
 
 import { getOfferSize } from '../Models/offersModel';
 import { getCampaignSize } from '../Models/campaignsModel';
-import { getFp, setFp } from '../Models/fpModel';
+import { delFp, getFp, setFp } from '../Models/fpModel';
 import { getLeadData, redshiftOffer } from '../Utils/dynamoDb';
 import { IRedshiftData } from '../Interfaces/redshiftData';
 
@@ -36,6 +36,9 @@ export class RecipeController extends BaseController {
     let fpCache: boolean = true;
     let resendLid: any;
 
+    if (isResendLid === 'del' && lid) {
+      await delFp(lid);
+    }
     if (isResendLid === 'yes' && lid) {
       const lidCache = await getFp(lid);
       if (lidCache) {

bf6b24057ddc5687a3e32aebda9cbb75f1aed3a1
dmytro miroshnykov <dmytro.miroshnykov@milkbox.com>
2022-05-11 16:36:06 -0400

    rm console log in setSqsDataToRedis

--- src/Utils/cache.ts
+++ src/Utils/cache.ts
@@ -1,13 +1,13 @@
 import consola from 'consola';
-import os from 'os';
+// import os from 'os';
 import { redis } from '../redis';
 import { influxdb } from './metrics';
 import { ISqsMessage } from '../Interfaces/sqsMessage';
 
-const computerName = os.hostname();
+// const computerName = os.hostname();
 
 export const setSqsDataToRedis = async (message: ISqsMessage): Promise<void> => {
-  consola.info(`Getting from recipe-api update, Set to redis ${message.type}ID:${message.id}, action:${message.action}, comments:${message.comments}, Computer name { ${computerName} }`);
+  // consola.info(`Getting from recipe-api update, Set to redis ${message.type}ID:${message.id}, action:${message.action}, comments:${message.comments}, Computer name { ${computerName} }`);
   try {
     if (message.action === 'updateOrCreate') {
       influxdb(200, 'local_redis_update_or_create');

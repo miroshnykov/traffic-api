dbcd16267c6574463a433b13b6fd077143b735d9
dmytro miroshnykov <dmytro.miroshnykov@milkbox.com>
2022-06-15 12:08:51 -0400

    PH-1112 setAggregatedOffersProportional by campaignId

--- src/Utils/aggregatedOffersProportional.ts
+++ src/Utils/aggregatedOffersProportional.ts
@@ -2,11 +2,11 @@ import { redis } from '../redis';
 // eslint-disable-next-line import/no-cycle
 import { ICalcAggregatedOffer } from '../Services/offers/offersAggregated';
 
-export const setAggregatedOffersProportional = async (arr: ICalcAggregatedOffer[]) => {
+export const setAggregatedOffersProportional = async (campaignId: number, arr: ICalcAggregatedOffer[]) => {
   const format = JSON.stringify(arr);
-  await redis.set('aggregatedOffersProportional', format);
+  await redis.set(`aggregatedOffersProportional:${campaignId}`, format);
 };
-export const getAggregatedOffersProportional = async () => {
-  const response = await redis.get('aggregatedOffersProportional');
+export const getAggregatedOffersProportional = async (campaignId: number) => {
+  const response = await redis.get(`aggregatedOffersProportional:${campaignId}`);
   return response ? JSON.parse(response) : [];
 };
